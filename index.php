b7effd9299c874467962725c67fd57e6478d7d49
mikch06 <mail@kissel.ch>
2020-06-05 14:24:35 +0200

    add bootstrap, modify index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+<!DOCTYPE html>
 <html>
-   <body>
-      
+<head>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <title>Uploader!</title>
+</head>
+<body>
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
       <form action="script/upload.php" method="POST" enctype="multipart/form-data">
          <input type="file" name="image">
          <input type="submit" value="Upload!">
       </form>
-      
-   </body>
-</html>
+</body>
+</html> 
